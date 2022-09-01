41efbdd16d676f2854a438c6a74d6991d22d1646
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-01 16:29:57 -0700

    redo kata

--- case-maker.js
+++ case-maker.js
@@ -1,19 +1,32 @@
+// const camelCase = function(input) {
+//   let string = "";
+//   let foundSpace = false;
+//   for (let i = 0; i < input.length; i++) {
+//     if (input.charAt(i) != " ") {
+//       if (foundSpace) {
+//         string = string + input.charAt(i).toUpperCase();
+//         foundSpace = false;
+//       } else {
+//         string = string + input.charAt(i);
+//       }
+//     } else if (input.charAt(i) === " ") {
+//       foundSpace = true;
+//     }
+//   }
+//   return string;
+// };
+
 const camelCase = function(input) {
-  let string = "";
-  let foundSpace = false;
-  for (let i = 0; i < input.length; i++) {
-    if (input.charAt(i) != " ") {
-      if (foundSpace) {
-        string = string + input.charAt(i).toUpperCase();
-        foundSpace = false;
-      } else {
-        string = string + input.charAt(i);
-      }
-    } else if (input.charAt(i) === " ") {
-      foundSpace = true;
+  let result = "";
+  const data = input.split(" ");
+  for (let i = 0; i < data.length; i++) {
+    if (i === 0) {
+      result += data[i];
+    } else {
+      result += data[i].charAt(0).toUpperCase() + data[i].slice(1);
     }
   }
-  return string;
+  return result;
 };
 
 console.log(camelCase("this is a string"));
