e3fa5b38f27cd81b36970bbd069feebd737bb7ca
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-01 16:20:14 -0700

    redo kata

--- conditional-sum.js
+++ conditional-sum.js
@@ -1,22 +1,37 @@
-const conditionalSum = function(values, condition) {
-  let result = 0;
+// const conditionalSum = function(values, condition) {
+//   let result = 0;
   
-  if (condition === "even") {
-    for (let i = 0; i < values.length; i++) {
-      if (values[i] % 2 === 0) {
-        result += values[i];
+//   if (condition === "even") {
+//     for (let i = 0; i < values.length; i++) {
+//       if (values[i] % 2 === 0) {
+//         result += values[i];
+//       }
+//     }
+//   } else if (condition === "odd") {
+//     for (let i = 0; i < values.length; i++) {
+//       if (values[i] % 2 !== 0) {
+//         result += values[i];
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+const conditionalSum = function(values, condition) {
+  let sum = 0;
+  for (let i = 0; i < values.length; i++) {
+    if (condition === "even") {
+      if ((values[i] % 2) === 0) {
+        sum += values[i];
       }
-    }
-  } else if (condition === "odd") {
-    for (let i = 0; i < values.length; i++) {
-      if (values[i] % 2 !== 0) {
-        result += values[i];
+    } else {
+      if ((values[i] % 2) !== 0) {
+        sum += values[i];
       }
     }
   }
-  return result;
+  return sum;
 };
-
 
 console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
 console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
