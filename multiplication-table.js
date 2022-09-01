bbfb7a4b6127d1278bf8c133783207e953129705
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-01 16:37:24 -0700

    redo kata

--- multiplication-table.js
+++ multiplication-table.js
@@ -1,16 +1,29 @@
+// const multiplicationTable = function(maxValue) {
+//   let table = "";
+//   let number = 0;
+//   for (let i = 1; i < maxValue + 1; i++) {
+//     for (let j = 1; j < maxValue + 1; j++) {
+//       number = i * j;
+//       table = table + number + " ";
+//     }
+//     table = table + "\n";
+//   }
+//   return table;
+// };
+
 const multiplicationTable = function(maxValue) {
   let table = "";
   let number = 0;
-  for (let i = 1; i < maxValue + 1; i++) {
-    for (let j = 1; j < maxValue + 1; j++) {
+  for (let i = 1; i <= maxValue; i++) {
+    for (let j = 1; j <= maxValue; j++) {
       number = i * j;
-      table = table + number + " ";
+      table += number + " ";
     }
     table = table + "\n";
   }
   return table;
 };
 
-console.log(multiplicationTable(2));
+console.log(multiplicationTable(1));
 console.log(multiplicationTable(5));
 console.log(multiplicationTable(10));
