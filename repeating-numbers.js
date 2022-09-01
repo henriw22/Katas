ce8b6fb6ef4fb4942890f6c128235a275f69bd49
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-01 16:20:59 -0700

    redo kata

--- repeating-numbers.js
+++ repeating-numbers.js
@@ -1,16 +1,29 @@
-const repeatNumbers = function(data) {
-  let result = "";
+// const repeatNumbers = function(data) {
+//   let result = "";
 
-  for (let i = 0; i < data.length; i++) { 
-    for (let j = 0; j < data[i][1]; j++) {
-      result = result + data[i][0];
+//   for (let i = 0; i < data.length; i++) { 
+//     for (let j = 0; j < data[i][1]; j++) {
+//       result = result + data[i][0];
+//     }
+//     if (i !== data.length - 1) {
+//       result = result + ", ";
+//     }
+//   }
+//   return result;
+// }
+
+const repeatNumbers = function(data) {
+  let numbers = "";
+  for (let x = 0; x < data.length; x++) {
+    for (let j = 0; j < data[x][1]; j++) {
+      numbers += data[x][0].toString();
     }
-    if (i !== data.length - 1) {
-      result = result + ", ";
+    if (x < data.length - 1) {
+      numbers += ", ";
     }
   }
-  return result;
-}
+  return numbers;
+};
 
 console.log(repeatNumbers([[1, 10]]));
 console.log(repeatNumbers([[1, 2], [2, 3]]));
