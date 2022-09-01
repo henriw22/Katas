6ce28bd05df3f416d1d172cbf4a47460aab2b02c
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-01 16:20:50 -0700

    redo kata

--- percent-encoded-string.js
+++ percent-encoded-string.js
@@ -1,16 +1,20 @@
-const urlEncode = function(text) {
-  text = text.trim();
-  let String = "";
-    for (let i = 0; i < text.length; i++) {
-      if (text[i] === " ") {
-        String += "%20";
-      } else {
-        String += text[i];
-      }
-    }
-  return String;  
-};
+// const urlEncode = function(text) {
+//   text = text.trim();
+//   let String = "";
+//     for (let i = 0; i < text.length; i++) {
+//       if (text[i] === " ") {
+//         String += "%20";
+//       } else {
+//         String += text[i];
+//       }
+//     }
+//   return String;  
+// };
 // return text.trim().replace(/ /g, '%20')
+
+const urlEncode = function(text) {
+  return text.trim().replace(/ /g, '%20');
+};
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
