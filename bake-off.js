7a0c9ff7b4ca3d234bef2bf2ab52b7877c1563cb
Henri William <83308210+henriw22@users.noreply.github.com>
2022-09-01 18:48:29 -0700

    redo kata

--- bake-off.js
+++ bake-off.js
@@ -1,39 +1,49 @@
-const ingredientCheck = function(bakery, ingredient) {
-    //checking if the ingredients matches with any ingredieng in the bakery
-    for (let i = 0; i < bakery.length; i++) {
-        if (ingredient === bakery[i]) {
-            return true;
+// const ingredientCheck = function(bakery, ingredient) {
+//     //checking if the ingredients matches with any ingredieng in the bakery
+//     for (let i = 0; i < bakery.length; i++) {
+//         if (ingredient === bakery[i]) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+//     // to go through recipes array
+//     for (let i = 0; i < recipes.length; i++) {
+//         const ingredient1 = recipes[i].ingredients[0];
+//         const ingredient2 = recipes[i].ingredients[1];
+//         //if an ingredient in position 0 of the recipe array matches with any in bakeryA
+//         if(ingredientCheck(bakeryA, ingredient1)) {
+//             //if an ingredient in position 1 of the recipe array matches with any in bakeryB, can be combined with the if statement above with &&
+//             if (ingredientCheck(bakeryB, ingredient2)) {
+//                 return recipes[i].name;
+//             }
+//         // if an ingredient in position 1 of the recipe array matches with any in Bakery B, just a reverse of the above nested if statement.
+//         } else if (ingredientCheck(bakeryB, ingredient1)) {
+//             if (ingredientCheck(bakeryA, ingredient2)) {
+//                 return recipes[i].name;
+//             }
+//         }
+//     }
+//     return "Recipe not found";
+// }
+
+// can combine the whole if statements above into one line if statement with this code
+// if((ingredientCheck(bakeryA, ingredient1) && ingredientCheck(bakeryB, ingredient2)) ||  (ingredientCheck(bakeryB, ingredient1) && ingredientCheck(bakeryA, ingredient2))) {
+//     return recipes[i].name;
+// }
+
+
+
+const chooseRecipe = function (bakeryA, bakeryB, recipes) {
+    for (let recipe of recipes) {
+        const ingredients = recipe.ingredients;
+        if ((bakeryA.includes(ingredients[0]) && bakeryB.includes(ingredients[1])) || (bakeryB.includes(ingredients[0]) && bakeryA.includes(ingredients[1]))) {
+                return recipe.name;
         }
     }
-    return false;
 }
-
-const chooseRecipe = function(bakeryA, bakeryB, recipes) {
-    // to go through recipes array
-    for (let i = 0; i < recipes.length; i++) {
-        const ingredient1 = recipes[i].ingredients[0];
-        const ingredient2 = recipes[i].ingredients[1];
-        //if an ingredient in position 0 of the recipe array matches with any in bakeryA
-        if(ingredientCheck(bakeryA, ingredient1)) {
-            //if an ingredient in position 1 of the recipe array matches with any in bakeryB, can be combined with the if statement above with &&
-            if (ingredientCheck(bakeryB, ingredient2)) {
-                return recipes[i].name;
-            }
-        // if an ingredient in position 1 of the recipe array matches with any in Bakery B, just a reverse of the above nested if statement.
-        } else if (ingredientCheck(bakeryB, ingredient1)) {
-            if (ingredientCheck(bakeryA, ingredient2)) {
-                return recipes[i].name;
-            }
-        }
-    }
-    return "Recipe not found";
-}
-
-    // can combine the whole if statements above into one line if statement with this code
-    // if((ingredientCheck(bakeryA, ingredient1) && ingredientCheck(bakeryB, ingredient2)) ||  (ingredientCheck(bakeryB, ingredient1) && ingredientCheck(bakeryA, ingredient2))) {
-    //     return recipes[i].name;
-    // }
-
 
 let bakeryA = ['saffron', 'eggs', 'tomato paste', 'coconut', 'custard'];
 let bakeryB = ['milk', 'butter', 'cream cheese'];
